41848bdee875676d0bb04fd257c976ead35758fa
unknown <alexander.diego.2007@hotmail.com>
2020-04-04 02:19:41 -0500

    cambios en menu y footer

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ window.onscroll = () => {
   myFunction();
 };
 
-var navbar = document.getElementById('navbar');
+var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 myFunction = () => {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add('sticky');
+    navbar.classList.add("sticky");
   } else {
-    navbar.classList.remove('sticky');
+    navbar.classList.remove("sticky");
   }
 };
+
+document.getElementById("getfulldate").innerHTML = new Date().getFullYear();
+
+AOS.init();
