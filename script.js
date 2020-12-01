4e643635a1f2b2a09d65e4bb7b97824647c9fdd6
diego41071 <alexander.diego.2007@hotmail.com>
2020-11-30 19:22:46 -0500

    cambios

--- script.js
+++ script.js
@@ -1,68 +1,24 @@
 document.getElementById("getfulldate").innerHTML = new Date().getFullYear();
 
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {
-        myIndex = 1;
-    }
-    x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 8000); // Change image every 2 seconds
-}
-
-var myIndex1 = 0;
-carousel1();
-
-function carousel1() {
-    var i;
-    var x = document.getElementsByClassName("mySlides1");
-
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    myIndex1++;
-    if (myIndex1 > x.length) {
-        myIndex1 = 1;
-    }
-    x[myIndex1 - 1].style.display = "block";
-    setTimeout(carousel1, 8000); // Change image every 2 seconds
-}
-
 window.onscroll = () => {
-    myFunction();
+  myFunction();
 };
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-myFunction = () => {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
-};
-
 function modal() {
-    $(".ui.modal.modal1").modal("show");
+  $(".ui.modal.modal1").modal("show");
 }
 
 function modal1() {
-    $(".ui.modal.modal2").modal("show");
+  $(".ui.modal.modal2").modal("show");
 }
 
 function modal2() {
-    $(".ui.modal.modal3").modal("show");
+  $(".ui.modal.modal3").modal("show");
 }
 
 function modal3() {
-    $(".ui.modal.modal4").modal("show");
+  $(".ui.modal.modal4").modal("show");
 }
